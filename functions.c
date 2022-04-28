cda1fd981579bd8e91494a4861d35fd011855e06
L0ud3r <playdagamehd@gmail.com>
2022-04-28 14:27:09 +0100

    Inicio funcionalidade comando 'stat' (exemplo copiado, por adaptar)

--- functions.c
+++ functions.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <dirent.h>
 #include "functions.h"
 
@@ -166,7 +167,34 @@ int apagar(char* ficheiro){
  * @return int 
  */
 int informar(char* ficheiro){
-
+if(argc != 2)    
+        return 1;
+ 
+    struct stat fileStat;
+    if(stat(argv[1],&fileStat) < 0)    
+        return 1;
+ 
+    printf("Information for %s\n",argv[1]);
+    printf("---------------------------\n");
+    printf("File Size: \t\t%d bytes\n",fileStat.st_size);
+    printf("Number of Links: \t%d\n",fileStat.st_nlink);
+    printf("File inode: \t\t%d\n",fileStat.st_ino);
+ 
+    printf("File Permissions: \t");
+    printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+    printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+    printf("\n\n");
+ 
+    printf("The file %s a symbolic link\n", (S_ISLNK(fileStat.st_mode)) ? "is" : "is not");
+ 
     return 1;
 }
 
